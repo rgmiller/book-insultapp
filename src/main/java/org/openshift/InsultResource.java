7af5041b6b0dc4b3bfda5c4b60644912207fe423
Ross Miller <rgmiller@ornl.gov>
2018-05-04 10:10:51 -0400

    Fix typos in java file

--- src/main/java/org/openshift/InsultResource.java
+++ src/main/java/org/openshift/InsultResource.java
@@ -3,14 +3,14 @@ package org.openshift;
 import java.util.HashMap;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
-import javax.ws.rs.PATH;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 @RequestScoped
 @Path("/insult")
 public class InsultResource {
 
-  @Get()
+  @GET()
   @Produces("application/json")
   public HashMap<String,String> getInsult() {
   HashMap<String, String> theInsult = new HashMap<String, String>();
